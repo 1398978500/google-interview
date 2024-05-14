942117535f14a29ee2b5879c1523b05a2eb5d3c2
wlw <1398978500@qq.com>
2024-05-14 11:41:42 +0800

    完善compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-# 没有build则创建
-[ -d "build" ] || mkdir build
-cd build && cmake ../practice && make
 
-if [[ $1 == "test" ]]
+# 没有build则创建
+if [[ $# < 1 || $1 == "" ]]
 then
-    make test
+    # 编译
+    [ -d "build" ] || mkdir build
+    cd build && cmake ../practice && make -j4
+elif [[ $1 == "test" ]]
+then
+    # 执行gtest
+    [ -d "build" ] && cd build && make test
+elif [[ $1 == "clean" ]]
+then
+    # 清理make缓存
+    [ -d "build" ] && cd build && make clean
+elif [[ $1 == "cleanall" ]]
+then
+    # 清理cmake和make缓存
+    [ -d "build" ] && rm build -r
+else
+    echo "./compile [argv]"
+    echo "argv可选:"
+    echo "  没有参数:编译"
+    echo "  test: 执行所有gtest相关可执行文件(需要先编译)"
+    echo "  clean: 清理make缓存"
+    echo "  cleanall:清理cmake和make缓存"
 fi
 
